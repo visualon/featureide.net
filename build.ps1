8865469145f8fdc8b693cbce3e1db7e993da9038
Michael Kriese <michael.kriese@visualon.de>
2017-06-21 14:24:42 +0200

    Added initial solution.

---
+++ build.ps1
@@ -0,0 +1,15 @@
+﻿param(
+
+    [string] $ikvmc = "c:\tools\ikvm-8.1.5717.0\bin\ikvmc.exe",
+    [string] $version = "3.2.0",
+    [string] $assemblyversion = "3.0.0",
+    [string] $pre = $null
+)
+
+Write-Output "Packing files" | Out-Host
+
+if($pre) {
+    $version += "-" + $pre
+}
+
+nuget pack featureide.csproj -build -version $version -OutputDirectory bin -properties "IKVMC=$ikvmc;Version=$version;AssemblyVersion=$assemblyversion"
