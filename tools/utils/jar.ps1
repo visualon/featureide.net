ca5cce4a7b9b8971020e47d06ed03b370e7e9b0e
Michael Kriese <michael.kriese@visualon.de>
2022-06-22 07:30:31 +0200

    build: missing dash

--- tools/utils/jar.ps1
+++ tools/utils/jar.ps1
@@ -21,7 +21,7 @@ function get-jar {
       # $uri = "https://raw.githubusercontent.com/FeatureIDE/FeatureIDE/v$version/plugins/de.ovgu.featureide.fm.core/lib/$name.jar"
       $n = $name -replace "org.sat4j.", ""
       $baseUri = "https://repository.ow2.org/nexus/content/repositories/releases/org/ow2/sat4j"
-      $uri = "$baseUri/org.ow2.sat4j.$n/$version/org.ow2.sat4j.$n$version.jar"
+      $uri = "$baseUri/org.ow2.sat4j.$n/$version/org.ow2.sat4j.$n-$version.jar"
     }
 
     "Download jar $name@$version"
