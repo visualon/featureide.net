ad76830b1a8fdfe5bbe3924b17f29e07a102b8e2
Michael Kriese <michael.kriese@visualon.de>
2023-03-27 12:41:01 +0200

    feat: migrate

--- tools/get-jars.ps1
+++ tools/get-jars.ps1
@@ -3,33 +3,20 @@
 param(
   [Parameter()]
   [string]
-  $version,
-
-  [Parameter()]
-  [bool]
-  $IkvmDebug
+  $version
 )
 
 . $PSScriptRoot/utils/index.ps1
 
-$target = "libs"
-
+$libs = "libs"
 $version = get-version -version $version
 $jarVersion = get-jar-version -version $version
 
-if ($version.Contains('-')) {
-  $IkvmDebug = $true
+if (Test-Path $libs) {
+  Remove-Item $libs -Recurse -Force
 }
+New-Item $libs -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 
-if (Test-Path $target) {
-  Remove-Item $target -Recurse -Force
-}
-
-New-Item $target -ItemType Directory -Force | Out-Null
-
-
-
-Write-Output "Downloading jars for version $jarVersion" | Out-Host
 get-jar -name 'de.ovgu.featureide.fm' -version $jarVersion
 get-jar -name 'org.sat4j.core' -version $SAT4J_VERSION
 get-jar -name 'org.sat4j.pb' -version $SAT4J_VERSION
@@ -37,12 +24,3 @@ copy-jar -name 'de.ovgu.featureide.fm' -version $jarVersion -target $libs
 copy-jar -name 'org.sat4j.core' -version $SAT4J_VERSION -target $libs
 copy-jar -name 'org.sat4j.pb' -version $SAT4J_VERSION -target $libs
 
-Write-Output "Compiling jars for version $version" | Out-Host
-
-dotnet build
-
-Write-Output "Packing files" | Out-Host
-
-
-nuget pack featureide.nuspec -OutputDirectory bin -version $version
-ThrowOnNativeFailure
